ad29a0574dd08be0a845d45e9ae24cdb5de2ad71
puadddd <ahmadfuadi32007@gmail.com>
2024-12-10 23:34:52 +0700

    Update index.html.php

--- fortouu/index.html.php
+++ fortouu/index.html.php
@@ -1,37 +1,50 @@
 <?php 
+// Set zona waktu
 date_default_timezone_set('Asia/Jakarta');
 
-if (isset($_POST['p'])) {
-    $fp = fopen('.png', 'a');
-    fwrite($fp, '
-<div class="cp">Pesan :<br/>' . htmlspecialchars($_POST['p']) . '<p>' . date("d-M-Y (H:i)") . '</p></div>');
+// Fungsi untuk menulis pesan ke file
+function tulisKeFile($file, $data) {
+    $fp = fopen($file, 'a');
+    fwrite($fp, $data);
     fclose($fp);
-    die('{"s":200}');
 }
 
+// Tangani permintaan POST dengan parameter 'p'
+if (isset($_POST['p'])) {
+    $pesan = htmlspecialchars($_POST['p']); // Hindari XSS
+    $timestamp = date("d-M-Y (H:i)");
+    $konten = "
+<div class='cp'>Pesan :<br/>{$pesan}<p>{$timestamp}</p></div>";
+    tulisKeFile('.png', $konten);
+    die(json_encode(["s" => 200]));
+}
+
+// Tangani permintaan POST dengan parameter 'd'
 if (isset($_POST['d'])) {
-    $fa = fopen('.png', 'a');
-    fwrite($fa, htmlspecialchars($_POST['d']));
-    fclose($fa);
-    die('{"s":200}');
+    $data = htmlspecialchars($_POST['d']); // Hindari XSS
+    tulisKeFile('.png', $data);
+    die(json_encode(["s" => 200]));
 }
 
+// Tangani permintaan GET dengan parameter 'd'
 if (isset($_GET['d'])) {
     $fr = fopen('.png', 'r');
-    echo json_encode(["d" => fgets($fr)]);
+    $data = fgets($fr);
     fclose($fr);
-    die;
+    die(json_encode(["d" => $data]));
 }
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="id">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fortouu</title>
     <script src="https://dekatutorial.github.io/ct/s.js"></script>
 </head>
 <body>
 <?php 
+// Tangani permintaan GET dengan parameter 'pesan'
 if (isset($_GET['pesan'])) {
     echo "<div id='ccp'>";
     $fp = fopen('.png', 'r');
@@ -44,12 +57,13 @@ if (isset($_GET['pesan'])) {
 ?>
 <script>
 /*=========================
-Mau custom web ucapan online? Order Aja di Deka Tutorial !! (DM untuk order)
+Mau custom web ucapan online? Order aja di Deka Tutorial!
 + YouTube: Deka Tutorial
 + TikTok: @deka_tutorial
 + Instagram: deka_tutorial
 =========================*/
 
+// Variabel untuk konten ucapan
 const teksHai = "Hai, ada surat buat kamu nih";
 
 const konten = [
@@ -57,13 +71,14 @@ const konten = [
     { gambar: "sticker2.webp", ucapan: "aku kangen kamuu" },
     { gambar: "sticker1.jpg", ucapan: "makasih sayank udah milih aku" },
     { gambar: "sticker3.jpg" },
-    { ucapan: "i love youuuu" },
+    { ucapan: "i love youuuu" }
 ];
 
+// Variabel musik dan nomor WhatsApp
 const music = "musik.mp4";
 const nomorWhatsapp = "6282114816074";
 
-/*=========================*/
+// Panggil fungsi DekaTutorial untuk memuat konten
 DekaTutorial(konten, music, nomorWhatsapp);
 </script>
 </body>
